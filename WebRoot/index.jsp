98272f9e84fa399124c06648921761f169f769cf
lingzg <elvis1005@yeah.net>
2018-01-20 18:33:55 +0800

    model列表、新增、编辑功能

--- WebRoot/index.jsp
+++ WebRoot/index.jsp
@@ -1,13 +1,87 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<c:redirect url="http://127.0.0.1:8080/gongdi/refresh.jsp"></c:redirect>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:set var="contextPath" value="${pageContext.request.contextPath}"></c:set>
 <!DOCTYPE html>
 <html>
 <head>
-
+	<title>流程模型列表</title>
+	<script type="text/javascript"
+		src="${contextPath}/editor-app/libs/jquery_1.11.0/jquery.min.js"></script>
+	<style type="text/css">
+	button {
+		margin: 5px;
+	}
+	table {
+		border-collapse: collapse;
+	}
+	table, th, td {
+		border: 1px solid black;
+	}
+	td {
+  		text-align:left;
+  		vertical-align:bottom;
+  		padding:10px;
+  	}
+  	th{
+  		background-color:gray;
+  	}
+	</style>
 </head>
-
 <body>
+	<h1>流程模型列表</h1>
+	<button onclick="add();">新增</button>
+	<table>
+		<thead>
+			<tr>
+				<th>名称</th>
+				<th>描述</th>
+				<th>版本</th>
+				<th>创建时间</th>
+				<th>最后修改时间</th>
+				<th>状态</th>
+				<th>操作</th>
+			</tr>
+		</thead>
+		<tbody>
 
+		</tbody>
+	</table>
 </body>
+
+<script type="text/javascript">
+	$(function() {
+		loadData();
+	});
+	function loadData() {
+		$.getJSON('${contextPath}/model/findModels', function(data) {
+			console.log(data);
+			var i, len, tr, description = '', status = '';
+			if (data && data.length > 0) {
+				for (i = 0, len = data.length; i < len; i++) {
+					if (data[i].metaInfo) {
+						description = JSON.parse(data[i].metaInfo).description;
+					}
+					status = data[i].deploymentId ? '已部署' : '未部署';
+					tr = '<tr>' +
+							'<td>' + data[i].name + '</td>' +
+							'<td>' + description + '</td>' +
+							'<td>' + data[i].version + '</td>' +
+							'<td>' + data[i].createTime + '</td>' +
+							'<td>' + data[i].lastUpdateTime + '</td>' +
+							'<td>'+ status + '</td>' +
+							'<td><a href="javascript:;" onclick="edit('+ data[i].id + ');" title="编辑">编辑</a></td>' +
+						 '</tr>';
+					$('tbody').append(tr);
+				}
+			}
+		});
+	}
+	function add() {
+		window.location.href = "${contextPath}/model/create?name=process&key=process2&description=process";
+	}
+	function edit(id) {
+		window.location.href = "${contextPath}/modeler.html?modelId=" + id;
+	}
+</script>
 </html>
--- src/org/activiti/rest/editor/ModuleController.java
+++ src/org/activiti/rest/editor/ModuleController.java
@@ -1,5 +1,7 @@
 package org.activiti.rest.editor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -20,40 +23,48 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Controller
 @RequestMapping("/model")
 public class ModuleController {
-	
+
 	private Logger logger = LoggerFactory.getLogger(ModuleController.class);
-	
+
 	@Autowired
 	private RepositoryService repositoryService;
-	
+
 	@RequestMapping(value = "create")
-	  public void create(@RequestParam("name") String name, @RequestParam("key") String key, @RequestParam("description") String description,
-	          HttpServletRequest request, HttpServletResponse response) {
-	    try {
-	      ObjectMapper objectMapper = new ObjectMapper();
-	      ObjectNode editorNode = objectMapper.createObjectNode();
-	      editorNode.put("id", "canvas");
-	      editorNode.put("resourceId", "canvas");
-	      ObjectNode stencilSetNode = objectMapper.createObjectNode();
-	      stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
-	      editorNode.put("stencilset", stencilSetNode);
-	      Model modelData = repositoryService.newModel();
-	 
-	      ObjectNode modelObjectNode = objectMapper.createObjectNode();
-	      modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, name);
-	      modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
-	      description = StringUtils.defaultString(description);
-	      modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
-	      modelData.setMetaInfo(modelObjectNode.toString());
-	      modelData.setName(name);
-	      modelData.setKey(StringUtils.defaultString(key));
-	 
-	      repositoryService.saveModel(modelData);
-	      repositoryService.addModelEditorSource(modelData.getId(), editorNode.toString().getBytes("utf-8"));
-	 
-	      response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
-	    } catch (Exception e) {
-	      logger.error("´´½¨Ä£ÐÍÊ§°Ü£º", e);
-	    }
-	  }
+	public void create(@RequestParam("name") String name, @RequestParam("key") String key,
+			@RequestParam("description") String description, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode editorNode = objectMapper.createObjectNode();
+			editorNode.put("id", "canvas");
+			editorNode.put("resourceId", "canvas");
+			ObjectNode stencilSetNode = objectMapper.createObjectNode();
+			stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
+			editorNode.put("stencilset", stencilSetNode);
+			Model modelData = repositoryService.newModel();
+
+			ObjectNode modelObjectNode = objectMapper.createObjectNode();
+			modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, name);
+			modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+			description = StringUtils.defaultString(description);
+			modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
+			modelData.setMetaInfo(modelObjectNode.toString());
+			modelData.setName(name);
+			modelData.setKey(StringUtils.defaultString(key));
+
+			repositoryService.saveModel(modelData);
+			repositoryService.addModelEditorSource(modelData.getId(), editorNode.toString().getBytes("utf-8"));
+
+			response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
+		} catch (Exception e) {
+			logger.error("åˆ›å»ºæ¨¡åž‹å¤±è´¥ï¼š", e);
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "findModels")
+	public List<Model> findModels(HttpServletRequest request, HttpServletResponse response) {
+		List<Model> models = repositoryService.createModelQuery().
+				orderByLastUpdateTime().desc().list();
+		return models;
+	}
 }
